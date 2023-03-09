8789dbd217bb349c4263ac304233a7b8b5cc9f40
GabrielaMitu <gabrielaym@al.insper.edu.br>
2023-03-09 19:51:37 -0300

    updates para duvida

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+
 #include<iostream>
 #include<vector>
 #include<algorithm>
@@ -22,14 +23,14 @@ int main() {
     int n; // numero de filmes
     int m; // numero de categorias
     cin >> n >> m;
-    vector<filme> filmes(n);
 
-    vector<int> max_filmes(m); // numero max por categoria
+    vector<filme> filmes(n);
+    vector<int> max_filmes(m); // numero max de filmes por categoria
 
     for (int i = 0; i < m; i++) {
-        //input_file >> max_filmes[i];
         cin >> max_filmes[i];
     }
+
 
     for (int i = 0; i < n; i++) {
         cin >> filmes[i].inicio >> filmes[i].fim >> filmes[i].categoria;
@@ -38,17 +39,46 @@ int main() {
     // ORDENAÇÃO CRESCENTE DO HORARIO DE TERMINO DOS FILMES 
     sort(filmes.begin(), filmes.end(), my_compare);
 
-    int ans = 0; // número máximo de filmes que podem ser assistidos de acordo com as restrições de horários e número máximo por categoria
-    vector<int> cnt(m, 0); // armazena o número de filmes assistidos em cada categoria, sendo m o tamanho do vetor e o valor inicial de cada elemento é zero
+    int selQuant = 0; // número máximo de filmes que podem ser assistidos de acordo com as restrições de horários e número máximo por categoria
+    vector<int> cntCat(m, 0); // armazena o número de filmes assistidos em cada categoria, sendo m o tamanho do vetor e o valor inicial de cada elemento é zero
+    vector<filme> selected; // lista dos filmes selecionados
+
     for (int i = 0; i < n; i++) {
-      // Percorre os filmes em ordem crescente de horário de término e, para cada filme, verifica se ainda é possível assistir um filme daquela categoria
-        if (cnt[filmes[i].categoria-1] < max_filmes[filmes[i].categoria-1]) {
-            ans++;
-            cnt[filmes[i].categoria-1]++; // aumenta o numero de filme selecionado de cada categoria 
-        }
+      if (filmes[i].fim - filmes[i].inicio < 0) {
+          continue;
+      }
+
+      // Verifica se os filmes selecionadps não ultrapassam o máximo por categoria
+      if (cntCat[filmes[i].categoria-1] >= max_filmes[filmes[i].categoria-1]) {
+          continue;
+      }
+
+      cout << "catQuant: " << cntCat[filmes[i].categoria-1] << " " << "maxPorCat: " << max_filmes[filmes[i].categoria-1] << endl;
+
+      cntCat[filmes[i].categoria-1]++;
+
+      for (int j = i + 1; j < n; j++) {
+          if (filmes[j].inicio > filmes[i].fim) {
+              i = j - 1;
+              selQuant++;
+              selected.push_back(filmes[i]);
+              break;
+          }
+      }
     }
 
-    cout << ans << endl;
+    // -------- OUTPUT --------
+    cout << "Máximo de filmes que pode ser assistidos: " << selQuant << endl;
+
+    // filmes selecionados para assistir
+    for (int s = 0; s < selected.size(); s++) {
+      cout << selected[s].inicio << " " << selected[s].fim << " " << selected[s].categoria << endl;
+    }
+
 
   return 0;
 }
+
+// CMD:
+// g++ -o filmesM filmesM.cpp
+// ./filmesM < "input.txt"
