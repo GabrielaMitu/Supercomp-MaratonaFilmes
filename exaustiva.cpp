36df0f69d41f53ce44fb50160c7618ea03da938d
GabrielaMitu <gabrielaym@al.insper.edu.br>
2023-05-21 14:16:01 -0300

    exaustiva pronta

--- exaustiva.cpp
+++ exaustiva.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<vector>
 #include<algorithm>
+#include <list>
 using namespace std;
 
 // STRUCT DO FILME
@@ -10,110 +11,138 @@ struct filme {
     int categoria;
 };
 
-// usados: conseguir controlar depois quais itens que eu coloquei na mochila
-double maratonaFilmes(int m, vector<filme> filmes, vector<filme>& assistidos, vector<filme>& melhor, vector<int> max_filmes, vector<int> cntCat, vector<int> maratona) {
-    int inicio = 0;
-    int fim = 0;
-    int categoria;
-    double sem_f = 0.0, com_f = 0.0; // sem_i = sem itens; com_i = com itens
-    vector<filme> filmes2 = filmes;
-    vector<int> cntCat2 = cntCat;
-    vector<int> maratona2 = maratona;
-
-    // verifica se o vetor de items esta vazio ou capacidade é zero
-    if (filmes.empty() || m == 0) {
-        return 0;
+// FUNCAO DE ORDENACAO CRESCENTE DO FIM DOS FILMES
+bool my_compare(filme a, filme b) {
+    if (a.fim != b.fim) {
+        return a.fim < b.fim;
+    } else {
+        return a.inicio < b.inicio;
     }
-
-    // dois braços da recursao:
-    // verifica se o primeiro filme da lista de filmes tem uma categoria que não esteja no valor máximo e não madruga e não começa e termina no msm horario
-    if (cntCat[filmes[0].categoria - 1] < max_filmes[filmes[0].categoria - 1] && filmes[0].fim - filmes[0].inicio > 0) {
-        assistidos.push_back(filmes[0]);
-        inicio = filmes[0].inicio;
-        fim = filmes[0].fim;
-        categoria = filmes[0].categoria;
-
-        // SE O FILME DEVE ENTRAR NA MARATONA
-        bool disponivel = true;
-        for (int m = inicio; m <= fim; m++)
-        {
-            if (maratona[m] == 1)
-            {
-                disponivel = false;
-                break;
-            }
-        }
-
-        if (disponivel)
-        {
-            for (int m = inicio; m <= fim; m++)
-            {
-                maratona[m] = 1;
-            }
-            cntCat[filmes[0].categoria - 1]++;
-            filmes.erase(filmes.begin()); // apaga o primeiro item do vetor de filmes original
-            com_f = maratonaFilmes(m, filmes, assistidos, melhor, max_filmes, cntCat, maratona); // versão incluindo o item
-            //std::cout << "com_f: " << com_f << endl;
-        }
-    }
-    filmes2.erase(filmes2.begin());
-    sem_f = maratonaFilmes(m, filmes2, assistidos, melhor, max_filmes, cntCat2, maratona2); // versão não incluindo o filme
-    //std::cout << "sem_f: " << sem_f << endl;
-
-    int countMaratona = 0; // com filme
-    int countMaratona2 = 0; // sem filme
-    for (int i : maratona) {
-        if (i == 1) {
-            countMaratona += 1;
-        }
-    }
-    for (int j : maratona2) {
-        if (j == 1) {
-            countMaratona2 += 1;
-        }
-    }
-
-    if (countMaratona2 >= countMaratona) {
-        melhor = assistidos;
-    }
-
-    if (sem_f == com_f) {
-        return sem_f;
-    }
-    assistidos.clear();
-
-    return max(com_f, sem_f);
 }
 
 int main() {
-    int n = 0; // numero de filmes
-    int m = 0; // numero de categorias totais
-    cin >> n >> m;
-    vector<filme> assistidos, melhor;
-    vector<filme> filmes(n);     // Vetor dos filmes dados pelo input
-    vector<int> max_filmes(m);   // Máximo de filmes por categoria
-    vector<int> cntCat(m, 0);    // Armazena o número de filmes assistidos em cada categoria, sendo m o tamanho do vetor e o valor inicial de cada elemento é zero
-    vector<int> maratona(23, 0); // Relacionado à maratona inicializa todos os valores com zero. Muda para 1 se o tempo estiver ocupado
+
+    // DECLARAÇÃO DE VARIAVEIS E VETORES
+    int n; // Numero de filmes
+    int m; // Numero de categorias totais
+    cin >> n >> m; // Input das variáveis acima
+
+    int maratona = 0; // Posição em que a maratona se encontra
+    vector<filme> filmes(n); // Vetor dos filmes dados pelo input
+    vector<int> max_filmes(m); // Máximo de filmes por categoria
+    vector<int> cntCat(m, 0); // Armazena o número de filmes assistidos em cada categoria, sendo m o tamanho do vetor e o valor inicial de cada elemento é zero
+    vector<filme> selected; // Lista dos filmes selecionados para a maratona
+    vector<int> duracao(n); // Duracão de cada filme
+    vector<filme> tempF;    // Vetor temporario para armazenar os filmes que começam no mesmo horario
 
     // INPUT DO MÁXIMO DE FILMES POR CATEGORIA
-    for (int i = 0; i < m; i++)
-    {
+    for (int i = 0; i < m; i++) {
         cin >> max_filmes[i];
     }
 
-    // INPUTS DOS FILMES
-    for (int i = 0; i < n; i++)
-    {
+    // INPUTS DOS FILMES 
+    for (int i = 0; i < n; i++) {
         cin >> filmes[i].inicio >> filmes[i].fim >> filmes[i].categoria;
     }
+    
+    // ORDENAÇÃO CRESCENTE DO HORARIO DE TERMINO DOS FILMES 
+    sort(filmes.begin(), filmes.end(), my_compare);
 
-    cout << "Resultado " << maratonaFilmes(m, filmes, assistidos, melhor, max_filmes, cntCat, maratona) << "\n"; 
-    for (auto& el: melhor) {
-        cout << el.inicio << " " << el.fim << " " << el.categoria << "\n";
+    // PERCORRENDO TODOS OS FILMES
+    for (int i = 0; i < n; i++) {
+        duracao[i] = filmes[i].fim - filmes[i].inicio; // Duracao de cada filme
+
+        // FILTRO DE FILMES SE COMEÇAREM A NOITE E TERMINAR DE MADRUGADA NO PROX DIA
+        if (filmes[i].fim - filmes[i].inicio < 0) {
+            continue;
+        }
+
+        // FILTRO SE FILME SELECIONADO NÃO ULTRAPASSA O MAX POR CATEGORIA
+        if (cntCat[filmes[i].categoria-1] >= max_filmes[filmes[i].categoria-1]) {
+            continue;
+        }
+
+        // FILTRO SE FILME NÃO COMEÇA E TERMINA NO MESMO HORÁRIO
+        if (duracao[i] == 0) {
+            continue;
+        }
+
+        int inicioF = filmes[i].inicio; // Inicio do filme
+        vector<int> duracao2;
+        int best; // Melhor filme para assistir
+        bool found = false;
+        bool vaiprox = false;
+        int w = 0;
+        //std::cout << "oi :" << filmes[i].inicio << " " << filmes[i].fim << " " <<filmes[i].categoria << endl;
+        int j = 0;
+
+        // Coloca em um vetor todos os filmes que tem inicio no mesmo horario 
+        while (!found && !vaiprox) {
+            while (w < n) {
+                if (filmes[i + j].inicio == maratona + w) {
+                    tempF.push_back(filmes[i + j]);
+                    duracao2.push_back(filmes[i + j].fim - filmes[i + j].inicio);
+                    found = true;
+                    break;
+                }
+                if (filmes[i + j].inicio < maratona) {
+                    vaiprox = true;
+                    break;
+                }
+            w++;
+            }
+        j++;
+
+        }
+
+/* 
+        for (int s = 0; s < static_cast<int>(tempF.size()); s++) {
+            std::cout << tempF[s].inicio << " " << tempF[s].fim << " " << tempF[s].categoria << endl;
+        } */
+
+        if (found) {
+            
+            // Encontra o iterador para o menor valor usando min_element
+            auto iteradorMenor = std::min_element(duracao2.begin(), duracao2.end());
+
+            // Calcula a posição (índice) do menor valor usando distance
+            int bestF = std::distance(duracao2.begin(), iteradorMenor);
+            //std::cout << "bestF: " << bestF << endl;
+
+            cntCat[tempF[bestF].categoria-1]++; // Aumenta a quantidade de filmes em cada categoria
+            maratona = tempF[bestF].fim; // Atualiza a variavel de maratona para a próxima seleção
+            //std::cout << "aloo: " << tempF[bestF].fim << endl;
+            selected.push_back(tempF[bestF]); // Adiona o filme dos selecionados para a maratona
+            
+            tempF.clear(); // Limpa o vetor tempF
+            duracao2.clear(); // Limpa o vetor duracao2
+
+            //std::cout << "fim :" << filmes[i].inicio << " " << filmes[i].fim << " " << filmes[i].categoria << endl;
+        }
+
     }
+
+    // -------- OUTPUT (TIRAR PARA A ANALISE NO VALGRIND) --------    
+    // FILMES DA MARATONA
+    std::cout << "Quantidade de filmes da maratona: " << selected.size() << endl;
+    int duracaoT = 0;
+    std::cout << "Duração total dos filmes selecionados: ";
+    for (int s = 0; s < static_cast<int>(selected.size()); s++) {
+        duracaoT += (selected[s].fim - selected[s].inicio);
+    }
+    std::cout << duracaoT << endl;
+
+    std::cout << "----------" << endl;
+
+    // FILMES SELECIONADOS PARA ASSISTIR
+    std::cout << "Filmes da maratona: " << endl;
+    for (int s = 0; s < static_cast<int>(selected.size()); s++) {
+      std::cout << selected[s].inicio << " " << selected[s].fim << " " << selected[s].categoria << endl;
+    }
+
+
     return 0;
 }
-
-// cmd:
+// PARA RODAR NO CMD:
 // g++ -o exaustiva exaustiva.cpp
 // ./exaustiva < inputs/input1.txt
